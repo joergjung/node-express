6f9e991c62507115ffd29056b1b6ecc2310f597f
Joerg Jung <joerg-jung@web.de>
2016-04-04 15:41:56 +0200

    implement gulp serve

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+// get port from process environment (gulpfile.js) or use 5000
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -9,7 +10,7 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
-app.get('/books', function(req, res) {
+app.get('/booksy', function(req, res) {
     res.send('Hello from books');
 });
 
