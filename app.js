8d0bfa99245742e788577df9dd6de3a17a30b906
Joerg Jung <joerg-jung@web.de>
2016-04-04 11:55:06 +0200

    install nodemon and other minor changes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
-
 var app = express();
-
 var port = 5000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+});
+
+app.get('/books', function(req, res) {
+    res.send('Hello from books');
+});
 
 app.listen(5000, function(err) {
     console.log('running server on port' + port);
