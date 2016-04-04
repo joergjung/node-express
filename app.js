23a7be395882363e961b9ec333ad56c23d9cbe0d
Joerg Jung <joerg-jung@web.de>
2016-04-04 22:04:50 +0200

    add ejs

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/booksy', function(req, res) {
