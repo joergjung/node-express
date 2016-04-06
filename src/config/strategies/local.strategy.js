896e07821dec299b2f82cce6f6447f3f3bb59f3c
Joerg Jung <joerg-jung@web.de>
2016-04-06 21:24:13 +0200

    update local strategy

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function() {
     passport.use(new LocalStrategy({
@@ -8,10 +9,23 @@ module.exports = function() {
         passwordField: 'password'
     },
     function(username, password, done) {
-        var user = {
-            username: username,
-            password: password
-        };  
-        done(null, user); 
+        var url = 'mongodb://localhost:27017/libraryApp';
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('users');
+            collection.findOne({
+                username: username
+            },
+            function(err, results) {
+                if (results.password === password) {
+                    var user = results;
+                    done(null, user);
+                } else {
+                    // password wrong
+                    // error null - but user is false, so authRouter for signIn will failureRedirect to '/'
+                    done(null, false, {message: 'Bad password!'});
+                }
+            }
+            );
+        });
     }));
 };
